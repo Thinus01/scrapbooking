1dbf56384344339dccab4bb90836f1f68bb151b6
Thinus Van De Venter <thinusvdv2001@gmail.com>
2023-11-01 13:03:48 +0200

    Add selection shading to css concepts

--- src/components/main/CSS.js
+++ src/components/main/CSS.js
@@ -1,11 +1,13 @@
 // Home to all CSS styling fun
 import Con1 from "../sub/CSS/con1/con1";
+import Con2 from "../sub/CSS/con2/con2";
 
 export function CSS() {
   return <>
       <h1 class="CSSIntro offset-4">CSS Interesting Concepts</h1>
       <section class="CSSStyles offset-1 col-10">
         <Con1 />
+        <Con2 />
       </section>
   </>
 }
--- src/components/sub/CSS/con2/con2.js
+++ src/components/sub/CSS/con2/con2.js
@@ -3,9 +3,10 @@ import './con2.css';
 function Con2() {
   return (
     <>
-      <h1>Glowing Border</h1>
       <section class="CSSContainer">
-        
+        <div class="OneSided offset-3 col-6">
+          <h1>Selection Shading(hover/click)</h1>
+        </div>
       </section>
     </>
   )
