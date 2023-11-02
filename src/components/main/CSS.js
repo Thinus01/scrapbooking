d630721836a65eb8be26b43bcdc4d4f7858de168
Thinus Van De Venter <thinusvdv2001@gmail.com>
2023-11-02 03:25:34 +0200

    Add enlarge to css concepts

--- src/components/main/CSS.js
+++ src/components/main/CSS.js
@@ -1,13 +1,15 @@
 // Home to all CSS styling fun
 import Con1 from "../sub/CSS/con1/con1";
 import Con2 from "../sub/CSS/con2/con2";
+import Con3 from "../sub/CSS/con3/con3";
 
 export function CSS() {
   return <>
-      <h1 class="CSSIntro offset-4">CSS Interesting Concepts</h1>
+      <h1 class="CSSIntro">CSS Interesting Concepts</h1>
       <section class="CSSStyles offset-1 col-10">
         <Con1 />
         <Con2 />
+        <Con3 />
       </section>
   </>
 }
---
+++ src/components/sub/CSS/con3/con3.js
@@ -0,0 +1,15 @@
+import './con3.css';
+
+function Con3() {
+  return (
+    <>
+      <section class="CSSContainer">
+        <div class="Enlarge offset-3 col-6">
+          <h1>Enlarge(hover)</h1>
+        </div>
+      </section>
+    </>
+  )
+  }
+  
+  export default Con3;
